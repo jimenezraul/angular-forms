711e1a736b181f9c217336540118f5a6a1daec24
Raul <jimenezraul1981@gmail.com>
2025-01-23 00:26:04 -0500

    refactor login component to use reactive forms with improved validation handling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  login(formData: NgForm) {
-    if (formData.invalid) {
-      return;
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  get emailIsValid() {
+    return (
+      this.form.touched &&
+      this.form.controls.email.invalid &&
+      this.form.controls.email.dirty
+    );
+  }
+
+  get passwordIsValid() {
+    return (
+      this.form.touched &&
+      this.form.controls.password.invalid &&
+      this.form.controls.password.dirty
+    );
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
     }
-
-    const { email, password } = formData.form.value;
-
-    console.log(email, password);
-    formData.reset();
   }
 }
